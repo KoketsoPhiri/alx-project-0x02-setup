c2e9277428c33a0668335c68f270efd8985aa637
KoketsoPhiri <koketsophiri789@gmail.com>
2025-07-21 09:54:59 +0200

    added about and home page

---
+++ alx-project-0x02/pages/about/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-50">
+      <h1 className="text-5xl font-semibold text-green-700">About Us Page</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ alx-project-0x02/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-50">
+      <h1 className="text-5xl font-semibold text-blue-700">Welcome to the Home Page!</h1>
+    </div>
+  );
+};
+
+export default Home;
