ece964965dc2c61ef9c0a3ac3b2f7523e6b29529
KOKETSO PHIRI <120306832+KoketsoPhiri@users.noreply.github.com>
2025-07-21 12:28:26 +0200

    type CardProps added

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,6 +1,6 @@
 // components/common/Card.tsx
 import React from 'react';
-import { CardProps } from '@/interfaces'; // Import the CardProps interface
+import { type CardProps } from '@/interfaces'; // Import the CardProps interface
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
